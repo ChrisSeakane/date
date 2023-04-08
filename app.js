aef42c08f70c30b70c2582b55db772560d2de823
ChrisSeakane <81533380+ChrisSeakane@users.noreply.github.com>
2023-04-08 14:21:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.post(`/api/v1/synchronizer/data`, wrap(async (req, res) => {
                 items.push(item);
             });
             */
-            s = s.year(y)
+            s = s.year(year)
             console.log(s.leapYear()?366:365)
             for (let d = 1; d <= (s.leapYear()?3:3); d++) {
                 s = s.dayOfYear(d);
